06202a611fff3147138034ec956841d5c1bf7727
andress20 <andress20@gmail.com>
2021-02-04 16:28:42 -0500

    add useMutation

--- src/graphql/resolvers/Mutation.js
+++ src/graphql/resolvers/Mutation.js
@@ -2,9 +2,12 @@ const User = require('../../models/user.model')
 
 module.exports = {
   async createUser(_, { input }) {
-    const newuser = new User(input)
-    await newuser.save()
-      return newuser
+    try {
+      const user = await User.create(input)
+      return user
+    }catch(error) {
+      return error
+    }
   },
   async updateUser(_, { _id, input }) {
     return await User.findByIdAndUpdate(_id, input, {new: true, useFindAndModify: false})
--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -21,6 +21,7 @@ const typeDefs = gql`
   input UserInput {
     name: String!
     email: String!
+    password: String!
   }
 
   input UserInputUpdate {
